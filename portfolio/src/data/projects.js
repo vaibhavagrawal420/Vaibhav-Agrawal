c6a336a1fb9c84e584cda473e798a0a7d6347ac0
vaibhavaagrawal <vaibhavmanbrother@gmail.com>
2026-07-27 03:52:17 +0530

    Updated portfolio

--- portfolio/src/data/projects.js
+++ portfolio/src/data/projects.js
@@ -82,7 +82,7 @@ export const projects = [
       "/images/projects/snacknow-cover.png",
       // TODO: Add more screenshots
     ],
-    github: "https://github.com/vaibhavagrawal420/SanckNow1",
+    github: "https://github.com/vaibhavagrawal420/SanckNow",
     liveDemo: null, // TODO: Add live demo URL if available
     status: "Completed",
     year: "2024",
